0fd89020da47e1f0ec5923a24775e38890f04d70
EnzoZa <71082476+EnzoZa@users.noreply.github.com>
2023-01-01 19:18:28 +0100

    :sparkles: heatmap :sparkles:

--- flutter_application/lib/meteo.dart
+++ flutter_application/lib/meteo.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application/main.dart';
 import 'package:flutter_application/station.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 
 class Meteo extends StatefulWidget {
   final City city;
@@ -10,13 +11,8 @@ class Meteo extends StatefulWidget {
 
   @override
   Widget build(BuildContext context) {
-    var text = Text('Meteo ' + city.name + ' ' + city.station_name);
     
-    return Scaffold(
-      appBar: AppBar(
-        title: text, 
-      ),
-    );
+    return Container();
   }
   
   @override
@@ -25,6 +21,7 @@ class Meteo extends StatefulWidget {
 
 class _MeteoState extends State<Meteo> {
   var wheather_informations = [];
+  Map<DateTime, int> date_informations = {};
 
   @override
   initState() {
@@ -33,27 +30,68 @@ class _MeteoState extends State<Meteo> {
   } 
 
   Future refreshWeathers () async {
-    var url = 'https://www.data.corsica/api/records/1.0/search/?dataset=query-outfields-and-where-1-3d1-and-f-geojson&q=&facet=nom_dept&facet=nom_com&facet=nom_station&facet=x_l93&facet=y_l93&refine.nom_com='+widget.city.name+'&refine.nom_station='+widget.city.station_request_name+'&timezone=Europe%2FParis&rows=1&format=json&lang=fr';
+    var url = 'https://www.data.corsica/api/records/1.0/search/?dataset=query-outfields-and-where-1-3d1-and-f-geojson&q=&rows=-1&sort=date_debut&facet=nom_dept&facet=nom_com&facet=nom_station&facet=x_l93&facet=y_l93&refine.nom_com='+widget.city.name+'&refine.nom_station='+widget.city.station_request_name;
+    //+'&timezone=Europe%2FParis&rows=1&format=json&lang=fr';
     Uri uri = Uri.parse(url);
+    print(uri);
 
     final response = await http.get(uri);
     var data = json.decode(response.body);
+    //print(data);
     data = data['records'];
-    print(data);
     this.wheather_informations = [];
+    this.date_informations = {};
+    var valeur = 0;
     setState(() {
       for(var i = 0; i < data.length; i++){
-        this.wheather_informations.add(data[i]);
-        print(data[i]);
+         this.wheather_informations.add(data[i]);
+        //add data[i]['date_debut'], check if it's not already in the list
+        //if date_debut is not in the list, add it with his valeur data[i]['valeur']
+        //else add the new value valeur data[i]['valeur'] to the existing one
+        var temp_date = data[i]['fields']['date_debut'];
+        temp_date = temp_date.substring(0,10);
+        DateTime date = DateTime.parse(temp_date);
+        valeur = double.parse(data[i]['fields']['valeur']).round();
+        //print('Date: $date');
+        if(this.date_informations.containsKey(date))
+          // ignore: curly_braces_in_flow_control_structures
+          this.date_informations.update(date, (value) => value + valeur);
+        else
+          this.date_informations[date] = valeur;
+        
+        //print(valeur);
+
+       //print(data[i]['geometry']['coordinates']);
       }
+
+        //print(this.date_informations);
     });
   }
   
   @override
   Widget build(BuildContext context) {
-   var text = Text('Meteo ' + widget.city.name + ' ' + widget.city.station_name);
+   //var text = Text('Meteo ' + widget.city.name + ' ' + widget.city.station_name);
     
-    return Scaffold(
+    return HeatMap(
+            datasets: this.date_informations,
+            //startDate: date_informations.keys.first,
+            //endDate: date_informations.keys.last,
+            colorMode: ColorMode.opacity,
+            showText: false,
+            scrollable: true,
+            colorsets: {
+              1: Colors.red,
+              3: Colors.orange,
+              5: Colors.yellow,
+              7: Colors.green,
+              9: Colors.blue,
+              11: Colors.indigo,
+              13: Colors.purple,
+              22: Colors.pink,
+            },
+          );
+    /*
+    Scaffold(
       appBar: AppBar(
         title: text, 
       ), 
@@ -66,6 +104,7 @@ class _MeteoState extends State<Meteo> {
           }),
       )
     );
+    */
   } 
 
 }
